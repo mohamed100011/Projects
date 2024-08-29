4d78b1a6d78fecd37eb0b688ff6237c763d22194
Mohamed <mohamed.ibraahiim4321@gmail.com>
2024-08-29 16:30:48 +0300

    calculator Ending code

--- Tip_Calculator_Ending_Code.py
+++ Tip_Calculator_Ending_Code.py
@@ -1,1 +1,12 @@
-print("Hello")
+age = int(input("What's Are Your Age ? "))
+
+year = 90 - age
+
+days = year * 365
+
+weeks = days * 7
+
+months = year * 12
+
+
+print(f"You Have {days} Days And {weeks} Weeks And {months} Months .")
