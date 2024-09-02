f058c84a85f6d76bc6824460403a2fb22a50c165
Mohamed <mohamed.ibraahiim4321@gmail.com>
2024-09-02 15:17:44 +0300

    Average Hight And Highest score

---
+++ Average_Height.py
@@ -0,0 +1,23 @@
+student_height = input("Input a list from student height ").split()
+
+for n in range(0, len(student_height)):
+    student_height[n] = int(student_height[n])
+# print(student_height)
+
+
+########## highest_score #################
+
+# highest_score = 0
+# for student in student_height:
+#     if student > highest_score:
+#         highest_score = student
+# print(highest_score)
+
+
+################### average_height ##########################
+
+# total_height = sum(student_height)
+# number_of_student = len(student_height)
+# average_height = round(total_height / number_of_student)
+
+# print(average_height)
