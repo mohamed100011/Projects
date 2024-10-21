8ff9b2d12053709807b2444feed2e750f254a86f
Mohamed <mohamed.ibraahiim4321@gmail.com>
2024-10-21 16:36:51 +0300

    etch a sketch app

---
+++ etch_A_sketch_app.py
@@ -0,0 +1,39 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+
+
+def move_forward():
+    tim.forward(10)
+
+
+def move_backward():
+    tim.backward(10)
+
+
+def move_left():
+    new_tim = tim.heading() + 10
+    tim.setheading(new_tim)
+
+
+def move_right():
+    new_tim = tim.heading() - 10
+    tim.setheading(new_tim)
+
+
+def clear():
+    tim.clear()
+    tim.penup()
+    tim.home()
+    tim.pendown()
+
+
+screen.listen()
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="a", fun=move_left)
+screen.onkey(key="d", fun=move_right)
+screen.onkey(key="c", fun=clear)
+
+screen.exitonclick()
